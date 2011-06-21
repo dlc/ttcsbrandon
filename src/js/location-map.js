5ca0fdff05148aa24330c0a3013d092aba8e071a
Darren Chamberlain <dlc@sevenroot.org>
2011-06-20 23:56:41 -0400

    Added location stuff
      o A Google Map indicating our locations
    
      o A JS file that generates the map data (for easy loading)
    
      o The config is all in the global variable locations.
    
      o Removed location information from the schedules page.

---
+++ src/js/location-map.js
@@ -0,0 +1,27 @@
+[% META type = 'text' %]
+var iw = new google.maps.InfoWindow();
+var m = new google.maps.Map(document.getElementById("map"), {
+    zoom: 8,
+    center: new google.maps.LatLng(28.022244, -81.732857),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+});
+
+[% FOR loc IN locations %] 
+// [% loc.title %]
+var [% loc.id %] = new google.maps.Marker({
+    position: new google.maps.LatLng([% loc.lat %], [% loc.lng %]),
+    map: m,
+    title: "[% loc.title %]"
+});
+
+google.maps.event.addListener([% loc.id %], "click", function () {
+    var c = "<div id=\"map-[% loc.id %]\" style=\"height: 100px; width: 400px; \"><strong>[% loc.title %]</strong>";
+[% IF loc.addr %]      c += "<br/><a href=\"http://maps.google.com/maps?q=[% loc.addr | uri | html %]\" target=\"_blank\">[% loc.addr %]</a>";[% END %]
+    c += "<br/>[% loc.desc %]";
+    c += "<br/><a href=\"[% site.uris.schedule %]#[% loc.id %]\">Schedule</a></div>";
+[% IF loc.nonmember %]      c += "<br/><em>Non-member instruction and participation available</em>";[% END %]
+
+    iw.setContent(c);
+    iw.open(m, [% loc.id %]);
+});
+[% END %]
