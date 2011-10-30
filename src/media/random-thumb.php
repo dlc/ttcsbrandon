885c6a032cb1d3a9648a79784248606858a38cf8
Darren Chamberlain <dlc@sevenroot.org>
2011-10-30 09:55:30 -0400

    Make this emit a full link+image snippet.

--- src/media/random-thumb.php
+++ src/media/random-thumb.php
@@ -3,6 +3,9 @@
 
     PROCESS 'photos';
 %]<?php
+$thumb_width = defined($thumb_width) ? $thumb_width : "95";
+$thumb_height = defined($thumb_height) ? $thumb_height : "64";
+
 $photos = array(
 [% FOREACH photo IN photos %]
     array(
@@ -11,9 +14,9 @@ $photos = array(
         'ext'   => '[% photo.ext %]'
     ),
 [% END %]);
-?>[% site.uris.media %]<?php
+?><a href="[% site.uris.media %]<?php
 
-$sel = $photos[ rand(0, (count($photos) - 1)) ];
+            $sel = $photos[ rand(0, (count($photos) - 1)) ];
         
-echo $sel['name'];
-?>/<?php # kill final newline ?>
+            echo $sel['name'];
+?>/"/><img src="[% site.uris.photos %]/<?php echo $sel['date']; ?>/<?php echo $sel['name']; ?>.<?php echo $sel['ext']; ?>" height="<?php echo $thumb_height; ?>" width="<?php echo $thumb_width; ?>"/></a>
