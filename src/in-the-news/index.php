359b74627439065608fa38a04b33fbb0b5b45d7c
Darren Chamberlain <dlc@sevenroot.org>
2011-10-30 10:43:35 -0400

    Comment these out, for now.

--- src/in-the-news/index.php
+++ src/in-the-news/index.php
@@ -5,10 +5,10 @@
 
 <div id="main" class="container_12">
     <div id="aggregated-news" class="grid_4">
-<?php include "ttcs-feeds.php"; ?>
+<!--<?php include "ttcs-feeds.php"; ?>-->
     </div>
     <div id="from-around-the-web" class="grid_4">
-<?php include "search-feed.php"; ?>
+<!--<?php include "search-feed.php"; ?>-->
     </div>
     <div id="twitter" class="grid_4">
 <?php include "twitter-widget.php"; ?>
