2c9ee3dca5a2eae7f8322f423df8384ac7131337
Darren Chamberlain <dlc@sevenroot.org>
2012-01-02 22:43:42 -0500

    Fix workshops link.

--- src/events/index.php
+++ src/events/index.php
@@ -8,7 +8,8 @@
 
 [% events(100, "events_html") %]
 
-  <h2><a href="[% site.uris.workshops %]">2011 Workshop Schedule</a></h2>
+  <!--h2><a href="[% site.uris.workshops %]2012/">2012 Workshop Schedule</a></h2-->
+  <h2><a href="[% site.uris.workshops %]2011/">2011 Workshop Schedule</a></h2>
 </div>
 <div id="photo">
     <a href="[% site.uris.pdfs %]/2012-01-07-openhouse.pdf"><img src="[% site.uris.images %]/thumbs/2012-01-07-openhouse.jpg" /></a>
