230319eb0312a5c1c8ba1656eec15baa491bc54a
Darren Chamberlain <dlc@sevenroot.org>
2012-01-08 08:55:45 -0500

    Post-openhouse updates, including a bunch of info on next week's workshop.

--- src/events/index.php
+++ src/events/index.php
@@ -4,13 +4,11 @@
       description   = 'Events and Happenings'
       atom_version  = 'events.atom'
 %]
-<div id="main" class="grid_7">
+<div id="main" class="container_12">
+    <a href="[% site.uris.pdfs %]/2012-01-14-workshop.pdf"><img src="[% site.uris.images %]/thumbs/2012-01-14-workshop.jpg" class="floater"/></a>
 
 [% events(100, "events_html") %]
 
   <!--h2><a href="[% site.uris.workshops %]2012/">2012 Workshop Schedule</a></h2-->
   <h2><a href="[% site.uris.workshops %]2011/">2011 Workshop Schedule</a></h2>
-</div>
-<div id="photo">
-    <a href="[% site.uris.pdfs %]/2012-01-07-openhouse.pdf"><img src="[% site.uris.images %]/thumbs/2012-01-07-openhouse.jpg" /></a>
 </div>
--- src/index.php
+++ src/index.php
@@ -3,17 +3,12 @@
       type  = 'html'
 %]
 <div id="main" class="grid_12">
-  <div class="container_12">
 
-    <div class="grid_5">
-      <a href="[% site.uris.pdfs %]/2012-01-07-openhouse.pdf"><img src="[% site.uris.images %]/thumbs/2012-01-07-openhouse.jpg" /></a>
+[% PROCESS "welcome" %]
 
-      <hr/>
+        <hr/>
 
-      <p id="20111101"><span class="leader">The Empty Step</span> &middot; The <a href="http://dunedin.florida.usa.taoist.org/pdf/201201issue30.pdf">January 2012 edition</a> is now available!  Previous issues are also <a href="http://dunedin.florida.usa.taoist.org/archive.php">available from the archive</a>.</p>
-    </div>
-
-    <div class="grid_7">
+      <a href="[% site.uris.pdfs %]/2012-01-14-workshop.pdf"><img src="[% site.uris.images %]/thumbs/2012-01-14-workshop.jpg" class="floater"/></a>
 
       <h2>Upcoming Branch Events</h2>
 
@@ -22,18 +17,11 @@
       <p>See more events on the <a href="[% site.uris.events %]">events page</a>.</p>
       <hr/>
 
-      <p><span class="leader">The Fung Loy Kok Institute of Taoism</span> has released a revised International Taoist Tai Chi Centre schedule for 2012. Want to know when the Chinese New Year banquet at the Toronto Centre is? (January 7.) Need to make plans for CIT Week in Orangeville? (August 5-10.) <a href="http://ittcs.files.wordpress.com/2011/10/2012-schedule-11-10-30-v3.pdf">Download the schedule</a>.</p>
+      <p id="p-emptystep"><span class="leader">The Empty Step</span> &middot; The <a href="http://dunedin.florida.usa.taoist.org/pdf/201201issue30.pdf">January 2012 edition</a> is now available!  Previous issues are also <a href="http://dunedin.florida.usa.taoist.org/archive.php">available from the archive</a>.</p>
+      <hr/>
+
+      <p id="p-fungloykok"><span class="leader">The Fung Loy Kok Institute of Taoism</span> has released a revised International Taoist Tai Chi Centre schedule for 2012. Want to know when the Chinese New Year banquet at the Toronto Centre is? (January 7.) Need to make plans for CIT Week in Orangeville? (August 5-10.) <a href="http://ittcs.files.wordpress.com/2011/10/2012-schedule-11-10-30-v3.pdf">Download the schedule</a>.</p>
 
       <hr />
-
-      <p id="p-x1"><span class="leader">Welcome</span> to the Brandon Florida branch of the <a href="http://www.usa.taoist.org/">[% abbr.ttcsusa %]</a>.</p>
-
-      <p id="p-x2">Taoist Tai Chi&reg; internal art of [% w("taijiquan") %] is a low impact exercise designed to enhance health. We invite you to join us to learn this ancient Chinese gentle internal art emphasizing relaxation, stretching, health improvement and stress reduction.
-
-      <p id="p-x3">In more than 25 countries around the globe, people are enjoying the many benefits of practicing Taoist Tai Chi&reg; taijiquan in the friendly atmosphere of our volunteer, non-profit organization.</p>
-
-      <p id="p-x4">Experience <a href="[% site.uris.schedule %]">one of our classes</a> for yourself and discover a genuine path for health and tranquility.</p>
-
-  </div>
 </div>
 
