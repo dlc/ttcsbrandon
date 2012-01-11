26cdc5a569b7750f513ece45d209b0c113b1ff78
Darren Chamberlain <dlc@sevenroot.org>
2012-01-10 19:26:45 -0500

    Change body of 404 page.

--- src/errors/404.php
+++ src/errors/404.php
@@ -6,7 +6,15 @@
 
 <div id="main" class="grid_12">
   <h1>Page Missing</h1>
+  <!-- http://www.virginiaaquarium.com/about-us/Pages/404.aspx?from=/ -->
 
-  <p>Looks like you followed a bad link.  Oops.</p>
+    <p>Oops...</p>
+    <p>Sorry, the page you are looking (<strong><?php echo $_SERVER["REQUEST_URI"] ?></strong>) for does not exist. Possible reasons why: </p>
+    <ol>
+        <li>There is a typo in the web address.</li>
+        <li>This page has been moved to another location.</li>
+        <li>You are time traveler and know something we don't.</li>
+    </ol>
+
 
 </div>
