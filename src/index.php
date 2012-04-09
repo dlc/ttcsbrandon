2fa3e280af8ce0944d249e63abab94604e78c5ba
Darren Chamberlain <dlc@sevenroot.org>
2012-04-09 12:03:16 -0400

    Latest empty step.

--- src/index.php
+++ src/index.php
@@ -17,10 +17,8 @@
       <p>See more events on the <a href="[% site.uris.events %]">events page</a>.</p>
       <hr/>
 
-      <p id="p-emptystep"><span class="leader">The Empty Step</span> &middot; The <a href="http://dunedin.florida.usa.taoist.org/pdf/201201issue30.pdf">January 2012 edition</a> is now available!  Previous issues are also <a href="http://dunedin.florida.usa.taoist.org/archive.php">available from the archive</a>.</p>
+      <p id="p-emptystep"><span class="leader">The Empty Step</span> &middot; The <a href="http://dunedin.florida.usa.taoist.org/pdf/201204issue33.pdf">April 2012 edition</a> is now available!  Previous issues are also <a href="http://dunedin.florida.usa.taoist.org/archive.php">available from the archive</a>.</p>
       <hr/>
-
-      <p id="p-fungloykok"><span class="leader">The Fung Loy Kok Institute of Taoism</span> has released a revised International Taoist Tai Chi Centre schedule for 2012. Want to know when the Chinese New Year banquet at the Toronto Centre is? (January 7.) Need to make plans for CIT Week in Orangeville? (August 5-10.) <a href="http://ittcs.files.wordpress.com/2011/10/2012-schedule-11-10-30-v3.pdf">Download the schedule</a>.</p>
 
       <hr />
 </div>
