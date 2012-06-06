5212556ec9f3a75f5704f0614e6391a45ecba8fc
Darren Chamberlain <dlc@sevenroot.org>
2012-06-06 14:54:40 -0400

    Flyer for 2012/06/24 intensive.

--- src/index.php
+++ src/index.php
@@ -7,6 +7,10 @@
     <div class="grid_6">
 [% PROCESS "welcome" %]
 
+    <hr />
+
+            <p>This video was made during the recent workshop at the Grand Reopening and 20th Anniversary celebrations in Brandon, Florida. Don't just watch, get up, danyu and sing along!</p>
+            <p><span class='embed-youtube' style='text-align:center; display: block;'><iframe class='youtube-player' type='text/html' width='500' height='312' src='http://www.youtube.com/embed/jy4PdcN9MgE?version=3&#038;rel=1&#038;fs=1&#038;showsearch=0&#038;showinfo=1&#038;iv_load_policy=1&#038;wmode=transparent' frameborder='0'></iframe></span></p>
         <hr />
 
       <h2>Upcoming Branch Events</h2>
@@ -26,10 +30,7 @@
             <p>Our grand opening is currently featured on <a href="http://ittcs.wordpress.com/2012/06/03/brandon-florida-celebrates-grand-reopening-20th-anniversary-at-workshop/">The Tiger's Mouth</a>:</p>
             <blockquote>The Brandon, Florida, branch celebrated the completion of extensive renovations to our building with a Grand Reopening ribbon-cutting ceremony and refreshments Friday evening May 25, 2012. A blue lion danced in, bringing good fortune and good wishes from the International and National TTCS dignitaries in attendance. </blockquote>
             <p>Be sure to read <a href="http://ittcs.wordpress.com/2012/06/03/brandon-florida-celebrates-grand-reopening-20th-anniversary-at-workshop/">the whole article</a>. There are pictures as well!<p>
-    <hr />
-
-            <p>This video was made during the recent workshop at the Grand Reopening and 20th Anniversary celebrations in Brandon, Florida. Don't just watch, get up, danyu and sing along!</p>
-            <p><span class='embed-youtube' style='text-align:center; display: block;'><iframe class='youtube-player' type='text/html' width='500' height='312' src='http://www.youtube.com/embed/jy4PdcN9MgE?version=3&#038;rel=1&#038;fs=1&#038;showsearch=0&#038;showinfo=1&#038;iv_load_policy=1&#038;wmode=transparent' frameborder='0'></iframe></span></p>
+    <p><a href="[% site.uris.pdf %]/intensive-2012-06-24.pdf"><img src="[% site.uris.images %]/thumbs/intensive-2012-06-24.jpg" /></a></p>
     </div>
 </div>
 
