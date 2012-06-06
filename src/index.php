7d386e321e7128418da9de86d62a17ad379009d1
Darren Chamberlain <dlc@sevenroot.org>
2012-06-06 14:59:15 -0400

    Rearranging the home page.

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@
 %]
 <div id="main" class="container_12">
 
-    <div class="grid_6">
+    <div class="grid_7">
 [% PROCESS "welcome" %]
 
     <hr />
@@ -13,24 +13,27 @@
             <p><span class='embed-youtube' style='text-align:center; display: block;'><iframe class='youtube-player' type='text/html' width='500' height='312' src='http://www.youtube.com/embed/jy4PdcN9MgE?version=3&#038;rel=1&#038;fs=1&#038;showsearch=0&#038;showinfo=1&#038;iv_load_policy=1&#038;wmode=transparent' frameborder='0'></iframe></span></p>
         <hr />
 
-      <h2>Upcoming Branch Events</h2>
+      <h2>Upcoming Events</h2>
 
 [% events(3, "events_html") %]
 
       <p>See more events on the <a href="[% site.uris.events %]">events page</a>.</p>
 
 
-        <hr />
-
-      <p id="p-emptystep"><span class="leader">The Empty Step</span> &middot; The <a href="http://dunedin.florida.usa.taoist.org/pdf/201206issue35.pdf">June 2012 edition</a> is now available!  Previous issues are also <a href="http://dunedin.florida.usa.taoist.org/archive.php">available from the archive</a>.</p>
-
     </div>
-    <div class="grid_6">
+    <div class="grid_5">
 
             <p>Our grand opening is currently featured on <a href="http://ittcs.wordpress.com/2012/06/03/brandon-florida-celebrates-grand-reopening-20th-anniversary-at-workshop/">The Tiger's Mouth</a>:</p>
             <blockquote>The Brandon, Florida, branch celebrated the completion of extensive renovations to our building with a Grand Reopening ribbon-cutting ceremony and refreshments Friday evening May 25, 2012. A blue lion danced in, bringing good fortune and good wishes from the International and National TTCS dignitaries in attendance. </blockquote>
             <p>Be sure to read <a href="http://ittcs.wordpress.com/2012/06/03/brandon-florida-celebrates-grand-reopening-20th-anniversary-at-workshop/">the whole article</a>. There are pictures as well!<p>
-    <p><a href="[% site.uris.pdf %]/intensive-2012-06-24.pdf"><img src="[% site.uris.images %]/thumbs/intensive-2012-06-24.jpg" /></a></p>
+
+        <hr />
+
+            <p><a href="[% site.uris.pdf %]/intensive-2012-06-24.pdf"><img src="[% site.uris.images %]/thumbs/intensive-2012-06-24.jpg" /></a></p>
+
+        <hr />
+
+      <p id="p-emptystep"><span class="leader">The Empty Step</span> &middot; The <a href="http://dunedin.florida.usa.taoist.org/pdf/201206issue35.pdf">June 2012 edition</a> is now available!  Previous issues are also <a href="http://dunedin.florida.usa.taoist.org/archive.php">available from the archive</a>.</p>
     </div>
 </div>
 
