12b348ef81791bec09ce68e4b27305e0007640c9
Darren Chamberlain <dlc@sevenroot.org>
2012-06-25 08:42:35 -0400

    Rearrange front page.

--- src/index.php
+++ src/index.php
@@ -4,14 +4,14 @@
 %]
 <div id="main" class="container_12">
 
-    <div class="grid_7">
+    <div class="grid_5">
 [% PROCESS "welcome" %]
 
-    <hr />
-
-            <p>This video was made during the recent workshop at the Grand Reopening and 20th Anniversary celebrations in Brandon, Florida. Don't just watch, get up, danyu and sing along!</p>
-            <p><span class='embed-youtube' style='text-align:center; display: block;'><iframe class='youtube-player' type='text/html' width='500' height='312' src='http://www.youtube.com/embed/jy4PdcN9MgE?version=3&#038;rel=1&#038;fs=1&#038;showsearch=0&#038;showinfo=1&#038;iv_load_policy=1&#038;wmode=transparent' frameborder='0'></iframe></span></p>
         <hr />
+
+      <p id="p-emptystep"><span class="leader">The Empty Step</span> &middot; The <a href="http://dunedin.florida.usa.taoist.org/pdf/201206issue35.pdf">June 2012 edition</a> is now available!  Previous issues are also <a href="http://dunedin.florida.usa.taoist.org/archive.php">available from the archive</a>.</p>
+
+    <hr />
 
       <h2>Upcoming Events</h2>
 
@@ -21,7 +21,7 @@
 
 
     </div>
-    <div class="grid_5">
+    <div class="grid_7">
 
             <p>Our grand opening is currently featured on <a href="http://ittcs.wordpress.com/2012/06/03/brandon-florida-celebrates-grand-reopening-20th-anniversary-at-workshop/">The Tiger's Mouth</a>:</p>
             <blockquote>The Brandon, Florida, branch celebrated the completion of extensive renovations to our building with a Grand Reopening ribbon-cutting ceremony and refreshments Friday evening May 25, 2012. A blue lion danced in, bringing good fortune and good wishes from the International and National TTCS dignitaries in attendance. </blockquote>
@@ -29,11 +29,9 @@
 
         <hr />
 
-            <p><a href="[% site.uris.pdf %]/intensive-2012-06-24.pdf"><img src="[% site.uris.images %]/thumbs/intensive-2012-06-24.jpg" /></a></p>
+            <p>This video was made during the recent workshop at the Grand Reopening and 20th Anniversary celebrations in Brandon, Florida. Don't just watch, get up, danyu and sing along!</p>
+            <p><span class='embed-youtube' style='text-align:center; display: block;'><iframe class='youtube-player' type='text/html' width='500' height='312' src='http://www.youtube.com/embed/jy4PdcN9MgE?version=3&#038;rel=1&#038;fs=1&#038;showsearch=0&#038;showinfo=1&#038;iv_load_policy=1&#038;wmode=transparent' frameborder='0'></iframe></span></p>
 
-        <hr />
-
-      <p id="p-emptystep"><span class="leader">The Empty Step</span> &middot; The <a href="http://dunedin.florida.usa.taoist.org/pdf/201206issue35.pdf">June 2012 edition</a> is now available!  Previous issues are also <a href="http://dunedin.florida.usa.taoist.org/archive.php">available from the archive</a>.</p>
     </div>
 </div>
 
