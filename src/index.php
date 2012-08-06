4b394c1cc7044dd2fbf5dcbd7a65d07a1bfaf18d
Darren Chamberlain <dlc@sevenroot.org>
2012-08-05 20:33:53 -0400

    2012/09/08 openhouse flyer.

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@
 %]
 <div id="main" class="container_12">
 
-    <div class="grid_5">
+    <div class="grid_6">
 [% PROCESS "welcome" %]
 
         <hr />
@@ -21,16 +21,15 @@
 
 
     </div>
-    <div class="grid_7">
+    <div class="grid_6">
+
+            <a href="[% site.uris.pdf %]/2012-09-08-openhouse.pdf"><img src="[% site.uris.images %]/thumbs/2012-09-08-openhouse.jpg"></a>
+
+        <hr />
 
             <p>Our grand opening is currently featured on <a href="http://ittcs.wordpress.com/2012/06/03/brandon-florida-celebrates-grand-reopening-20th-anniversary-at-workshop/">The Tiger's Mouth</a>:</p>
             <blockquote>The Brandon, Florida, branch celebrated the completion of extensive renovations to our building with a Grand Reopening ribbon-cutting ceremony and refreshments Friday evening May 25, 2012. A blue lion danced in, bringing good fortune and good wishes from the International and National TTCS dignitaries in attendance. </blockquote>
             <p>Be sure to read <a href="http://ittcs.wordpress.com/2012/06/03/brandon-florida-celebrates-grand-reopening-20th-anniversary-at-workshop/">the whole article</a>. There are pictures as well!<p>
-
-        <hr />
-
-            <p>This video was made during the recent workshop at the Grand Reopening and 20th Anniversary celebrations in Brandon, Florida. Don't just watch, get up, danyu and sing along!</p>
-            <p><span class='embed-youtube' style='text-align:center; display: block;'><iframe class='youtube-player' type='text/html' width='500' height='312' src='http://www.youtube.com/embed/jy4PdcN9MgE?version=3&#038;rel=1&#038;fs=1&#038;showsearch=0&#038;showinfo=1&#038;iv_load_policy=1&#038;wmode=transparent' frameborder='0'></iframe></span></p>
 
     </div>
 </div>
