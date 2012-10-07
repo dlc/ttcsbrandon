dff996e2c5ace0363b6d5ef53ccc8b2e9ca06250
Darren Chamberlain <dlc@sevenroot.org>
2012-10-07 09:16:50 -0400

    Rearrage homepage.

--- src/index.php
+++ src/index.php
@@ -13,23 +13,19 @@
 
     <hr />
 
+            <p>Our grand opening is currently featured on <a href="http://ittcs.wordpress.com/2012/06/03/brandon-florida-celebrates-grand-reopening-20th-anniversary-at-workshop/">The Tiger's Mouth</a>:</p>
+            <blockquote>The Brandon, Florida, branch celebrated the completion of extensive renovations to our building with a Grand Reopening ribbon-cutting ceremony and refreshments Friday evening May 25, 2012. A blue lion danced in, bringing good fortune and good wishes from the International and National TTCS dignitaries in attendance. </blockquote>
+            <p>Be sure to read <a href="http://ittcs.wordpress.com/2012/06/03/brandon-florida-celebrates-grand-reopening-20th-anniversary-at-workshop/">the whole article</a>. There are pictures as well!<p>
+
+    </div>
+    <div class="grid_6">
+
       <h2>Upcoming Events</h2>
 
 [% events(5, "events_html") %]
 
       <p>See more events on the <a href="[% site.uris.events %]">events page</a>.</p>
 
-
-    </div>
-    <div class="grid_6">
-
-            <a href="[% site.uris.pdf %]/2012-09-08-openhouse.pdf"><img src="[% site.uris.images %]/thumbs/2012-09-08-openhouse.jpg"></a>
-
-        <hr />
-
-            <p>Our grand opening is currently featured on <a href="http://ittcs.wordpress.com/2012/06/03/brandon-florida-celebrates-grand-reopening-20th-anniversary-at-workshop/">The Tiger's Mouth</a>:</p>
-            <blockquote>The Brandon, Florida, branch celebrated the completion of extensive renovations to our building with a Grand Reopening ribbon-cutting ceremony and refreshments Friday evening May 25, 2012. A blue lion danced in, bringing good fortune and good wishes from the International and National TTCS dignitaries in attendance. </blockquote>
-            <p>Be sure to read <a href="http://ittcs.wordpress.com/2012/06/03/brandon-florida-celebrates-grand-reopening-20th-anniversary-at-workshop/">the whole article</a>. There are pictures as well!<p>
 
     </div>
 </div>
