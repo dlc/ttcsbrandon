02ce4c659cf63044df3382404a990b819bca9554
Darren Chamberlain <dlc@sevenroot.org>
2012-11-20 08:28:35 -0500

    Rearrange the front page to add the holiday flyer.

--- src/index.php
+++ src/index.php
@@ -8,10 +8,11 @@
 [% PROCESS "welcome" %]
 
         <hr />
+      <h2>Upcoming Events</h2>
 
-[% PROCESS "emptystep" %]
+[% events(5, "events_html") %]
 
-    <hr />
+      <p>See more events on the <a href="[% site.uris.events %]">events page</a>.</p>
 
             <p>Our grand opening is currently featured on <a href="http://ittcs.wordpress.com/2012/06/03/brandon-florida-celebrates-grand-reopening-20th-anniversary-at-workshop/">The Tiger's Mouth</a>:</p>
             <blockquote>The Brandon, Florida, branch celebrated the completion of extensive renovations to our building with a Grand Reopening ribbon-cutting ceremony and refreshments Friday evening May 25, 2012. A blue lion danced in, bringing good fortune and good wishes from the International and National TTCS dignitaries in attendance. </blockquote>
@@ -20,11 +21,10 @@
     </div>
     <div class="grid_6">
 
-      <h2>Upcoming Events</h2>
+[% PROCESS "emptystep" %]
 
-[% events(5, "events_html") %]
-
-      <p>See more events on the <a href="[% site.uris.events %]">events page</a>.</p>
+    <hr />
+      [% pdf_thumb("2012-12-09-holiday-party") %]
 
 
     </div>
