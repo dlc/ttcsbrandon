e1991048989f00faa33a36ee7a25a031da818a2f
Darren Chamberlain <dlc@sevenroot.org>
2012-12-03 11:10:45 -0500

    Remove Grand Opening announcement.

--- src/index.php
+++ src/index.php
@@ -14,11 +14,6 @@
 [% events(5, "events_html") %]
 
       <p>See more events on the <a href="[% site.uris.events %]">events page</a>.</p>
-        <hr />
-
-            <p>Our grand opening is currently featured on <a href="http://ittcs.wordpress.com/2012/06/03/brandon-florida-celebrates-grand-reopening-20th-anniversary-at-workshop/">The Tiger's Mouth</a>:</p>
-            <blockquote>The Brandon, Florida, branch celebrated the completion of extensive renovations to our building with a Grand Reopening ribbon-cutting ceremony and refreshments Friday evening May 25, 2012. A blue lion danced in, bringing good fortune and good wishes from the International and National TTCS dignitaries in attendance. </blockquote>
-            <p>Be sure to read <a href="http://ittcs.wordpress.com/2012/06/03/brandon-florida-celebrates-grand-reopening-20th-anniversary-at-workshop/">the whole article</a>. There are pictures as well!<p>
 
     </div>
     <div class="grid_6">
