2342422a9f0e28cdbc91ce75af39c6f693535fc1
Darren Chamberlain <dlc@sevenroot.org>
2013-01-05 08:06:10 -0500

    Include link to 2013 schedule.

--- src/events/index.php
+++ src/events/index.php
@@ -10,10 +10,7 @@
 [% events(100, "events_html") %]
     </div>
 
-    <!--div class="grid_5">
-      <a href="[% site.uris.pdfs %]/2012-05-25-grand-reopening.pdf"><img src="[% site.uris.images %]/thumbs/2012-05-25-grand-reopening.jpg" /></a>
-    </div-->
-
+  <h2><a href="[% site.uris.workshops %]2013/">2013 Workshop Schedule</a></h2>
   <!--h2><a href="[% site.uris.workshops %]2012/">2012 Workshop Schedule</a></h2-->
   <!--h2><a href="[% site.uris.workshops %]2011/">2011 Workshop Schedule</a></h2-->
 </div>
