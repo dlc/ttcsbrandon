11dc655d5bf2d31fada9bb2bb0f47fa4b1d4ecd5
Darren Chamberlain <dlc@sevenroot.org>
2013-01-05 08:20:54 -0500

    Workshop links

--- src/events/index.php
+++ src/events/index.php
@@ -10,7 +10,7 @@
 [% events(100, "events_html") %]
     </div>
 
-  <h2><a href="[% site.uris.workshops %]2013/">2013 Workshop Schedule</a></h2>
-  <!--h2><a href="[% site.uris.workshops %]2012/">2012 Workshop Schedule</a></h2-->
-  <!--h2><a href="[% site.uris.workshops %]2011/">2011 Workshop Schedule</a></h2-->
+  <h2><a href="[% site.uris.workshops %]2013/">2013 Workshop Schedule</a> (<a href="[% site.uris.pdfs %]/2013-usa-workshops.pdf">pdf</a>)</h2>
+  <h2><a href="[% site.uris.workshops %]2012/">2012 Workshop Schedule</a> (<a href="[% site.uris.pdfs %]/2012-usa-workshops.pdf">pdf</a>)</h2>
+  <h2><a href="[% site.uris.workshops %]2011/">2011 Workshop Schedule</a> (<a href="[% site.uris.pdfs %]/2011-usa-workshops.pdf">pdf</a>)</h2>
 </div>
