1f24615e0a2ebe1f3f97aee3fd82c8e8bb0f1bfe
Darren Chamberlain <dlc@sevenroot.org>
2013-01-12 21:33:43 -0500

    Remove openhouse stuffs.

--- src/index.php
+++ src/index.php
@@ -5,37 +5,20 @@
 <div id="main" class="container_12">
 
     <div class="grid_6">
-        <div style="border: 2px dashed maroon; background: #e0e0e0; text-align: center; ">
-            <h3 style="color: maroon;">NOTICE</h3>
-
-            <p>The ad in the Tampa Tribune was incorrect.</p>
-
-            <p>Our <a href="[% site.uris.pdfs %]/2013-01-13-openhouse.pdf" style="color: maroon;"><strong>Open House</strong></a> is on
-            <strong>Saturday, January 12, 2013.</strong></p>
-
-            <p>Brandon <strong>11:00-1:00</strong><br/>
-            Temple Terrace <strong>2:00-4:00</strong></p>
-        </div>
-
-        <hr />
-
 [% PROCESS "welcome" %]
 
         <hr />
+
+[% PROCESS "emptystep" %]
+
+    </div>
+    <div class="grid_6">
 
       <h2>Upcoming Events</h2>
 
 [% events(5, "events_html") %]
 
       <p>See more events on the <a href="[% site.uris.events %]">events page</a>.</p>
-
-    </div>
-    <div class="grid_6">
-
-[% PROCESS "emptystep" %]
-
-    <hr />
-      [% pdf_thumb("2013-01-13-openhouse") %]
 
     </div>
 </div>
