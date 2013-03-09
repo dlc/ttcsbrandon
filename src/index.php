0682b7cff3edac4d73dfc66c093f4dfcb389cd96
Darren Chamberlain <dlc@sevenroot.org>
2013-03-09 07:45:24 -0500

    Link to article on tbo.com.

--- src/index.php
+++ src/index.php
@@ -5,7 +5,10 @@
 <div id="main" class="container_12">
 
     <div class="grid_6">
-[% PROCESS "welcome" %]
+
+    <p id="p-1"><span class="leader">Tampa Tribune Article</span> &middot; John Ceballos from the Tampa Tribune <a href="http://www2.tbo.com/news/brandon/2013/mar/07/taoist-tai-chi-offers-a-variety-of-health-benefits-ar-652286/" title="&quot;Taoist Tai Chi offers a variety of health benefit&quot;, March 07, 2013">published an article about our center</a>, which features interviews with a few of our members, and provides a little history and a lot of exposure. There are even a few pictures.</p>
+
+    <p><a href="http://www2.tbo.com/news/brandon/2013/mar/07/taoist-tai-chi-offers-a-variety-of-health-benefits-ar-652286/" title="&quot;Taoist Tai Chi offers a variety of health benefit&quot;, March 07, 2013"><img src="[% site.uris.images %]/tbo.jpg" alt="Members practicing the tai chi form"></a></p>
 
         <hr />
 
@@ -14,9 +17,13 @@
     </div>
     <div class="grid_6">
 
+[% PROCESS "welcome" %]
+
+        <hr />
+
       <h2>Upcoming Events</h2>
 
-[% events(5, "events_html") %]
+[% events(3, "events_html") %]
 
       <p>See more events on the <a href="[% site.uris.events %]">events page</a>.</p>
 
