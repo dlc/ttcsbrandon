49d61ac371febb060df2a10fc58de80a3c129836
Darren Chamberlain <dlc@sevenroot.org>
2013-06-05 21:03:26 -0400

    Remove workshop poster and rearrange bits.

--- src/index.php
+++ src/index.php
@@ -14,23 +14,19 @@
 
     <p><a href="http://www2.tbo.com/news/brandon/2013/mar/07/taoist-tai-chi-offers-a-variety-of-health-benefits-ar-652286/" title="&quot;Taoist Tai Chi offers a variety of health benefit&quot;, March 07, 2013"><img src="[% site.uris.images %]/tbo.jpg" alt="Members practicing the tai chi form"></a></p>
 
-        <hr />
-
-[% PROCESS "emptystep" %]
-
     </div>
     <div class="grid_6">
 
-[% pdf_thumb("2013-05-25-international-workshop", "International Tai Chi Workshop, May 25-26, 2013") %]
-
-        <hr />
-
       <h2>Upcoming Events</h2>
 
-[% events(3, "events_html") %]
+[% events(5, "events_html") %]
 
       <p>See more events on the <a href="[% site.uris.events %]">events page</a>.</p>
 
+        <hr />
+
+
+[% PROCESS "emptystep" %]
 
 
     </div>
