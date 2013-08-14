36da7062350570e1e5eca8dcd94dbdf921758b58
Darren Chamberlain <dlc@sevenroot.org>
2013-08-14 11:43:57 -0400

    Rearrange home page.

--- src/index.php
+++ src/index.php
@@ -14,6 +14,12 @@
 
     <p><a href="http://ittcs.wordpress.com/2013/06/07/brandon-fl-hosts-international-workshop/" title="Brandon, FL, Hosts International Workshop"><img src="http://ittcs.files.wordpress.com/2013/06/dsc8670.jpg?w=460" alt="Members practicing the tai chi form" width="460" height="247"></a></p>
 
+      <h2>Upcoming Events</h2>
+
+[% events(5, "events_html") %]
+
+      <p>See more events on the <a href="[% site.uris.events %]">events page</a>.</p>
+
         <hr/>
 
 [% PROCESS "emptystep" %]
@@ -21,17 +27,11 @@
     </div>
     <div class="grid_5">
 
-[% pdf_thumb("2013-09-07-openhouse", "Open House") %]
+[% pdf_thumb("2013-08-24-intensive", "Instructor Intensive and Luncheon") %]
 
 <hr/>
 
-      <h2>Upcoming Events</h2>
-
-[% events(5, "events_html") %]
-
-      <p>See more events on the <a href="[% site.uris.events %]">events page</a>.</p>
-
-        <hr />
+[% pdf_thumb("2013-09-07-openhouse", "Open House") %]
 
 
 
