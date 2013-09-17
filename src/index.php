ec572ffb1df768c5393a0d627a073160916692db
Darren Chamberlain <dlc@sevenroot.org>
2013-09-17 08:07:01 -0400

    Remove old event flyers

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@
 %]
 <div id="main" class="container_12">
 
-    <div class="grid_7">
+    <div class="grid_6">
 
 [% PROCESS "welcome" %]
 
@@ -13,6 +13,9 @@
     <p id="p-1"><span class="leader"><a href="http://ittcs.wordpress.com/2013/06/07/brandon-fl-hosts-international-workshop/" title="Brandon, FL, Hosts International Workshop">Brandon, FL, Hosts International Workshop</a></span> &middot; <em>May 25 and 26 were special days here in the Tampa Bay area&mdash;they marked Brandon's International Taoist Tai Chi&reg; Workshop 2013, led by Jim Nicholson.</em> Read the rest of the article on the <a href="http://ittcs.wordpress.com/2013/06/07/brandon-fl-hosts-international-workshop/" title="Brandon, FL, Hosts International Workshop">ITTCS web site</a>.</p>
 
     <p><a href="http://ittcs.wordpress.com/2013/06/07/brandon-fl-hosts-international-workshop/" title="Brandon, FL, Hosts International Workshop"><img src="http://ittcs.files.wordpress.com/2013/06/dsc8670.jpg?w=460" alt="Members practicing the tai chi form" width="460" height="247"></a></p>
+
+    </div>
+    <div class="grid_6">
 
       <h2>Upcoming Events</h2>
 
@@ -23,16 +26,6 @@
         <hr/>
 
 [% PROCESS "emptystep" %]
-
-    </div>
-    <div class="grid_5">
-
-[% pdf_thumb("2013-08-24-intensive", "Instructor Intensive and Luncheon") %]
-
-<hr/>
-
-[% pdf_thumb("2013-09-07-openhouse", "Open House") %]
-
 
 
     </div>
