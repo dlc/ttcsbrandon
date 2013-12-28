459b997a695a53b60dc97fa0d6c46c4aef309c64
Darren Chamberlain <dlc@sevenroot.org>
2013-12-27 23:09:47 -0500

    Latest Open House.

--- src/index.php
+++ src/index.php
@@ -10,18 +10,16 @@
 
         <hr />
 
-    <p id="p-1"><span class="leader"><a href="http://ittcs.wordpress.com/2013/06/07/brandon-fl-hosts-international-workshop/" title="Brandon, FL, Hosts International Workshop">Brandon, FL, Hosts International Workshop</a></span> &middot; <em>May 25 and 26 were special days here in the Tampa Bay area&mdash;they marked Brandon's International Taoist Tai Chi&reg; Workshop 2013, led by Jim Nicholson.</em> Read the rest of the article on the <a href="http://ittcs.wordpress.com/2013/06/07/brandon-fl-hosts-international-workshop/" title="Brandon, FL, Hosts International Workshop">ITTCS web site</a>.</p>
+      <h2>Upcoming Events</h2>
 
-    <p><a href="http://ittcs.wordpress.com/2013/06/07/brandon-fl-hosts-international-workshop/" title="Brandon, FL, Hosts International Workshop"><img src="http://ittcs.files.wordpress.com/2013/06/dsc8670.jpg?w=460" alt="Members practicing the tai chi form" width="460" height="247"></a></p>
+      <p>See more events on the <a href="[% site.uris.events %]">events page</a>.</p>
+
+[% events(5, "events_html") %]
 
     </div>
     <div class="grid_6">
 
-      <h2>Upcoming Events</h2>
-
-[% events(5, "events_html") %]
-
-      <p>See more events on the <a href="[% site.uris.events %]">events page</a>.</p>
+[% pdf_thumb("2014-01-11-openhouse", "Open House") %]
 
         <hr/>
 
